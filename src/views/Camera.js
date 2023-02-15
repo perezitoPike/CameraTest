45c1b1cfd6a52138d947fe18d7112c53d8cf4030
Wilder Chura <wilderchur@gmail.com>
2023-02-15 05:42:43 -0400

    Version 3: Camera

--- src/views/Camera.js
+++ src/views/Camera.js
@@ -4,24 +4,113 @@ var canvasContext;
 var videoTag = document.getElementById('theVideo');
 var canvasTag = document.getElementById('theCanvas');
 var btnCapture = document.getElementById("btnCapture");
+var listaDeDispositivos = document.getElementById('listaDeDispositivos');
 
 videoTag.setAttribute('width', videoWidth);
 videoTag.setAttribute('height', videoHeight);
 canvasTag.setAttribute('width', videoWidth);
 canvasTag.setAttribute('height', videoHeight);
 
+
+const tieneSoporteUserMedia = () =>
+    !!(navigator.getUserMedia || (navigator.mozGetUserMedia || navigator.mediaDevices.getUserMedia) || navigator.webkitGetUserMedia || navigator.msGetUserMedia);
+const _getUserMedia = (...arguments) =>
+    (navigator.getUserMedia || (navigator.mozGetUserMedia || navigator.mediaDevices.getUserMedia) || navigator.webkitGetUserMedia || navigator.msGetUserMedia).apply(navigator, arguments);
+
+function obtenerDispositivos(){
+    return navigator.mediaDevices.enumerateDevices();
+}
+
+const limpiarSelect = ()=>{
+    for (let x = listaDeDispositivos.options.length; x >= 0; x--){
+        listaDeDispositivos.remove(x);
+    }
+};
+
+const llenarSelectConDispositivosDisponibles = () => {
+
+        limpiarSelect();
+        obtenerDispositivos()
+            .then(dispositivos => {
+                const dispositivosDeVideo = [];
+                dispositivos.forEach(dispositivo => {
+                    const tipo = dispositivo.kind;
+                    if (tipo === "videoinput") {
+                        dispositivosDeVideo.push(dispositivo);
+                    }
+                });
+    
+                if (dispositivosDeVideo.length > 0) {
+                    dispositivosDeVideo.forEach(dispositivo => {
+                        const option = document.createElement('option');
+                        option.value = dispositivo.deviceId;
+                        option.text = dispositivo.label;
+                        listaDeDispositivos.appendChild(option);
+                    });
+                }
+            });
+    }
+
+    const mostrarStream = idDeDispositivo => {
+        _getUserMedia({
+                video: {
+                    deviceId: idDeDispositivo,
+                }
+            },
+            (streamObtenido) => {    
+                stream = streamObtenido;
+    
+                videoTag.srcObject = stream;
+                videoTag.play();
+    
+                
+            }, (error) => {
+                console.log("Permiso denegado o error: ", error);
+                // $estado.innerHTML = "No se puede acceder a la cámara, o no diste permiso.";
+            });
+    }
+
 window.onload = () => {
-    navigator.mediaDevices.getUserMedia({
-        audio: false,
-        video: {
-            width: videoWidth,
-            height: videoHeight
+    if(!tieneSoporteUserMedia()){
+        alert("Not Camera Connected");
+        return;
+    }
+    llenarSelectConDispositivosDisponibles();
+    let stream;
+    obtenerDispositivos().then(dispositivos=>{
+        const dispositivosDeVideo = [];
+        dispositivos.forEach(function(dispositivo){
+            const tipo = dispositivo.kind;
+            if(tipo == "videoinput"){
+                dispositivosDeVideo.push(dispositivo);
+            }
+        });
+
+        if(dispositivosDeVideo.length > 0){
+            mostrarStream(dispositivosDeVideo[0].deviceId);
         }
-    }).then(stream => {
-        videoTag.srcObject = stream;
-    }).catch(e => {
-        document.getElementById('errorTxt').innerHTML = 'ERROR: ' + e.toString();
+    
+        listaDeDispositivos.onchange = () => {
+
+            if (stream) {
+                stream.getTracks().forEach(function(track) {
+                    track.stop();
+                });
+            }
+            mostrarStream(listaDeDispositivos.value);
+        }
     });
+    // navigator.mediaDevices.getUserMedia({
+    //     audio: false,
+    //     video: {
+    //         width: videoWidth,
+    //         height: videoHeight
+    //     }
+    // }).then(stream => {
+    //     videoTag.srcObject = stream;
+    // }).catch(e => {
+    //     document.getElementById('errorTxt').innerHTML = 'ERROR: ' + e.toString();
+    // });
 
     canvasContext = canvasTag.getContext('2d');
     
