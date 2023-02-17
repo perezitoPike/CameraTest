1e5209aaa0d56e72a23e98962120664d10e7f553
Wilder Chura <wilderchur@gmail.com>
2023-02-17 12:52:19 -0400

    Version 6.4.5

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
-if (process.argv[2] == undefined) {
-    global.console.log("Usage: node application.js <COM#>");
-    process.exit(1);
-}
-let portCom = process.argv[2];
+// if (process.argv[2] == undefined) {
+//     global.console.log("Usage: node application.js <COM#>");
+//     process.exit(1);
+// }
+// let portCom = process.argv[2];
 
 const express = require('express');
 const app = express();
@@ -35,23 +35,23 @@ app.use(require('./routes/index.routes'));
 const {SerialPort} = require('serialport');
 const {DelimiterParser} = require('@serialport/parser-delimiter');
 
-const port = new SerialPort({
-    path: portCom,
-    baudRate: 9600,
-});
+// const port = new SerialPort({
+//     path: portCom,
+//     baudRate: 9600,
+// });
 
-const parser = port.pipe(new DelimiterParser({delimiter: '\n'}));
+// const parser = port.pipe(new DelimiterParser({delimiter: '\n'}));
 
-parser.on('open', function () {
-    console.log('connection is opened');
-});
+// parser.on('open', function () {
+//     console.log('connection is opened');
+// });
 
-parser.on('data', function (data) {
-    io.emit('arduinoMessage', data.toString());
-});
+// parser.on('data', function (data) {
+//     io.emit('arduinoMessage', data.toString());
+// });
 
-parser.on('error', (err) => console.log(err));
-port.on('error', (err) => console.log(err));
+// parser.on('error', (err) => console.log(err));
+// port.on('error', (err) => console.log(err));
 
 //Socket Server
 socketServer(server);
